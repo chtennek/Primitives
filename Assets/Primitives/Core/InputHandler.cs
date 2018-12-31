dba74bcb675f2b5e9c1bb1fee3b47b139a88f69b
GoodKenneth <kenthesage@gmail.com>
2018-12-31 18:08:08 -0500

    Initial commit

---
+++ Assets/Primitives/Core/InputHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Rewired;
+
+public class InputHandler : MonoBehaviour
+{
+    public int playerId = 0;
+    private Player player;
+
+    private void Awake()
+    {
+        player = ReInput.players.GetPlayer(playerId);
+    }
+}
